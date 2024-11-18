e9fc395d4954771d30e14a6eda61a28779c27d07
weslleymartinss <weslleymartins1@hotmail.com>
2024-11-18 11:58:35 -0300

    add animation loading

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const ativar = document.querySelector('.ativar')
 const modal = document.querySelector('#modal')
 
+const entrar = document.querySelector('.entrar')
+const loading = document.querySelector('.loading')
+
+entrar.addEventListener('click', () => {
+    loading.classList.toggle('loading-animar')
+})
+
 
 window.addEventListener ('load', () => {
     modal.classList.toggle('animar')
