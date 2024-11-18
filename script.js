a706e796a5f6bd7ad0f6401fde59638ccfb54ace
weslleymartinss <weslleymartins1@hotmail.com>
2024-11-18 09:29:04 -0300

    animação

---
+++ script.js
@@ -0,0 +1,7 @@
+const ativar = document.querySelector('.ativar')
+const modal = document.querySelector('#modal')
+
+
+window.addEventListener ('load', () => {
+    modal.classList.toggle('animar')
+})
